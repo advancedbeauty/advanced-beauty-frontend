c5d107857c85a7e5f3013ad95dc5d2ca01319e02
Diwakar Jha <diwakarjha554@gmail.com>
2024-09-15 03:56:50 +0530

    almost perfected getintouch

--- components/ui/features/Section.tsx
+++ components/ui/features/Section.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 interface SectionProps {
     children: React.ReactNode;
     className?: string;
+    style?: React.CSSProperties;
 }
 
-const Section: React.FC<SectionProps> = ({ children, className }) => {
-    return <section className={`${className} w-full flex justify-center items-center`}>{children}</section>;
+const Section: React.FC<SectionProps> = ({ children, className, style }) => {
+    return <section className={`${className} w-full flex justify-center items-center`} style={ style }>{children}</section>;
 };
 
 export default Section;
