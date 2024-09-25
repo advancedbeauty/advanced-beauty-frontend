d13f68d2f3020850abdfa23d3cd79bca587ba291
Diwakar Jha <diwakarjha554@gmail.com>
2024-09-25 11:48:29 +0530

    add details

--- data/reviewsData.ts
+++ data/reviewsData.ts
@@ -2,7 +2,7 @@ export const ReviewsData = [
     {
         name: 'Sachin Sharma',
         img: '/noProfile.webp',
-        reviewText: 'It is Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error pariatur dicta deserunt distinctio tempora blanditiis accusantium ut aperiam labore dolore architecto consequuntur ad, ratione alias nemo aliquid voluptatem iusto ipsa? Nulla assumenda hic corporis voluptatibus tenetur obcaecati, explicabo maiores beatae consectetur at a esse saepe neque cumque? Minima, voluptate dolorum?',
+        reviewText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error pariatur dicta deserunt distinctio tempora blanditiis accusantium ut aperiam labore dolore architecto consequuntur ad, ratione alias nemo aliquid voluptatem iusto ipsa? Nulla assumenda hic corporis voluptatibus tenetur obcaecati, explicabo maiores beatae consectetur at a esse saepe neque cumque? Minima, voluptate dolorum?',
     },
     {
         name: 'Diwakar Jha',
