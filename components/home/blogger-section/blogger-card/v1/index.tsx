9e322fe979d25f23db1e96342aefe7cc4a4db201
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-01 03:06:19 +0530

    instagram post

--- components/home/blogger-section/blogger-card/v1/index.tsx
+++ components/home/blogger-section/blogger-card/v1/index.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 
 const BloggerCard = () => {
     return (
-        <div className='flex gap-5'>
-            <InstagramEmbed postUrl="https://www.instagram.com/reel/C-owXjRg3hq/" />
-            <InstagramEmbed postUrl="https://www.instagram.com/reel/C-owXjRg3hq/" />
-            <InstagramEmbed postUrl="https://www.instagram.com/reel/C-owXjRg3hq/" />
-            <InstagramEmbed postUrl="https://www.instagram.com/reel/C-owXjRg3hq/" />
+        <div className='flex gap-5 flex-wrap'>
+            <div className="max-w-lg mx-auto">
+                <InstagramEmbed postUrl="https://www.instagram.com/reel/C3xLL_HNXg2/" />
+            </div>
+            <div className="max-w-lg mx-auto">
+                <InstagramEmbed postUrl="https://www.instagram.com/reel/C63MARGPtRq/" />
+            </div>
+            <div className="max-w-lg mx-auto">
+                <InstagramEmbed postUrl="https://www.instagram.com/reel/C0k0wu5KDqg/" />
+            </div>
+            <div className="max-w-lg mx-auto">
+                <InstagramEmbed postUrl="https://www.instagram.com/reel/C3SVQInvk8t/" />
+            </div>
         </div>
     );
 };
--- components/home/faq-section/index.tsx
+++ components/home/faq-section/index.tsx
@@ -5,8 +5,11 @@ import React from 'react';
 const HomeFAQSection = () => {
     return (
         <Section className="py-20">
-            <Container>
-                FAQ Section
+            <Container className='w-full flex flex-col gap-10'>
+                    <div className="flex flex-col gap-1">
+                        <h2 className="font-bold text-2xl sm:text-3xl">Frequently Ask Questions</h2>
+                        <p className="font-semibold text-xl text-gray-600">Buy your favourite items</p>
+                    </div>
             </Container>
         </Section>
     );
