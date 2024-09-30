2046907010db0bedc7c9cf361d8bbcbc3cda4498
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-01 03:26:16 +0530

    instagram post

--- components/home/blogger-section/index.tsx
+++ components/home/blogger-section/index.tsx
@@ -1,13 +1,26 @@
 import Container from '@/components/ui/features/Container';
 import Section from '@/components/ui/features/Section';
 import React from 'react';
-import BloggerCard from './blogger-card/v1';
+import InstagramEmbed from '@/components/ui/instagram-embed';
 
 const HomeBloggerSection = () => {
     return (
         <Section className="py-16 md:py-20">
             <Container>
-                <BloggerCard />
+                <div className="flex gap-5 flex-wrap">
+                    <div className="max-w-lg mx-auto">
+                        <InstagramEmbed postUrl="https://www.instagram.com/reel/C3xLL_HNXg2/" />
+                    </div>
+                    <div className="max-w-lg mx-auto">
+                        <InstagramEmbed postUrl="https://www.instagram.com/reel/C63MARGPtRq/" />
+                    </div>
+                    <div className="max-w-lg mx-auto">
+                        <InstagramEmbed postUrl="https://www.instagram.com/reel/C0k0wu5KDqg/" />
+                    </div>
+                    <div className="max-w-lg mx-auto">
+                        <InstagramEmbed postUrl="https://www.instagram.com/reel/C3SVQInvk8t/" />
+                    </div>
+                </div>
             </Container>
         </Section>
     );
