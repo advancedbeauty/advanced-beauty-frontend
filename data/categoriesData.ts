b2a640942fddc375abb6df72fb5bf8a61dae1cee
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-06 07:16:10 +0530

    error solved

--- data/categoriesData.ts
+++ data/categoriesData.ts
@@ -2,13 +2,13 @@ export const categoriesData = [
     {
         id: 'services',
         name: 'Services',
-        href: 'http://localhost:3000/services',
+        href: '/services',
         items: ['Smartphones', 'Laptops', 'Tablets', 'Headphones', 'Smartwatches']
     },
     {
         id: 'shop',
         name: 'Shop',
-        href: 'http://localhost:3000/shop',
+        href: '/shop',
         items: ['T-shirts', 'Jeans', 'Dresses', 'Jackets', 'Shoes']
     },
 ]
