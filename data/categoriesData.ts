fd5cc9fdd03af3b85b8c094fa432187584961160
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-08 06:52:39 +0530

    next version change

--- data/categoriesData.ts
+++ data/categoriesData.ts
@@ -3,14 +3,14 @@ export const categoriesData = [
         id: 'services',
         name: 'Services',
         href: '/services',
-        img: '/temp/categories/service_png.png',
+        img: '/temp/categories/service_png.webp',
         items: ['Smartphones', 'Laptops', 'Tablets', 'Headphones', 'Smartwatches']
     },
     {
         id: 'shop',
         name: 'Shop',
         href: '/shop',
-        img: '/temp/categories/shop_png.png',
+        img: '/temp/categories/shop_png.webp',
         items: ['T-shirts', 'Jeans', 'Dresses', 'Jackets', 'Shoes']
     },
 ]
