2598cd9590dd6615dab346407b831cf3395fe470
diwakarjha <diwakarjha554@gmail.com>
2024-12-23 14:52:11 +0530

    solve order errors

--- components/admin/manage/orders/index.tsx
+++ components/admin/manage/orders/index.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import { DataTable } from './data-table';
 import { columns } from './columns';
 import { OrderStats } from './order-stats';
-import { OrderFilters } from './order-filters';
 import { getAdminOrders, getOrderStats } from '@/actions/order/order.actions';
 import { OrderStatus } from '@/types/order';
+import { formatUrlString } from '@/lib/formatUrlString';
 
 const ManageOrdersPage = async () => {
     const ordersData = await getAdminOrders({});
     const statsData = await getOrderStats();
 
     // Transform orders to match the Order type
-    const transformedOrders = ordersData.success && ordersData.orders
-    ? ordersData.orders.map(order => ({
-        ...order,
-        address: JSON.parse(order.address),
-        status: order.status as OrderStatus
-    }))
-    : [];
+    const transformedOrders =
+        ordersData.success && ordersData.orders
+            ? ordersData.orders.map((order) => ({
+                  ...order,
+                  address: JSON.parse(order.address),
+                  status: order.status as OrderStatus,
+              }))
+            : [];
 
     return (
         <div className="flex flex-col gap-4 p-4">
@@ -26,15 +27,22 @@ const ManageOrdersPage = async () => {
             {/* Stats Cards */}
             {statsData.success && statsData.stats && <OrderStats stats={statsData.stats} />}
 
-            {/* Filters */}
-            <OrderFilters />
-
             {/* Orders Table */}
             {ordersData.success && (
-                <DataTable 
-                    columns={columns} 
-                    data={transformedOrders} 
-                    pagination={ordersData.pagination} 
+                <DataTable
+                    columns={columns}
+                    data={transformedOrders.map((order) => ({
+                        ...order,
+                        customerName: order.user.name,
+                        customerAddress: `${order.address.fullName}, ${order.address.city}`,
+                        items: order.orderItems.map((item) => ({
+                            ...item,
+                            link: `/${item.itemType === 'SERVICE' ? 'services' : 'shop'}/${formatUrlString(
+                                item.itemName
+                            )}`,
+                        })),
+                    }))}
+                    pagination={ordersData.pagination}
                 />
             )}
         </div>
